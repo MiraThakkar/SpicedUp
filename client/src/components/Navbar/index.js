e31ad6b6d5802fcac46d820c1a62beb266b79b6b
Mira Thakkar <mthakkar84@gmail.com>
2020-05-28 23:14:34 -0400

    added form component in addrecipe page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -28,6 +28,7 @@ function Navbar () {
                     <a className="nav-link" href="#">About Us</a>
                 </li>
             </ul>
+            
         
         <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
--- client/src/pages/AddRecipe.js
+++ client/src/pages/AddRecipe.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BackButton } from "../components/Button";
-
+import { Input, TextArea, FormBtn } from "../components/Form";
+import Jumbotron from "../components/Jumbotron"
 
 import { Col, Row, Container } from "../components/Grid";
 
@@ -8,11 +9,68 @@ function Add() {
     const [recipeObject, setRecipeObject] = useState({})
 
     return (
-        <Col size="md-12">
-            <BackButton />
-        </Col>
+        <Container fluid>
 
         
+        <Row>
+            <Col size="md-12">
+                <BackButton />
+                <Jumbotron>
+                <h1>Add recipe from your kitchen</h1>
+                </Jumbotron>
+            </Col> 
+        </Row>
+        <Row>
+            <Col size= "md-8">
+            <form className = "ml-5">
+              <Input
+                // onChange={handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <Input
+                // onChange={handleInputChange}
+                name="author"
+                placeholder="Author (required)"
+              />
+               <Input
+                // onChange={handleInputChange}
+                name="time"
+                placeholder="Cooking time"
+              />
+               <Input
+                // onChange={handleInputChange}
+                name="serving"
+                placeholder="Serving"
+              />
+               <Input
+                // onChange={handleInputChange}
+                name="ingredients"
+                placeholder="Ingredients (required)"
+              />
+
+              
+              <TextArea
+                // onChange={handleInputChange}
+                name="direction"
+                placeholder= "Direction (required)"
+              />
+              <FormBtn
+                // disabled={!(formObject.author && formObject.title)}
+                // onClick={handleFormSubmit}
+              >
+                Save Recipe
+              </FormBtn>
+            </form>
+
+            </Col>
+        </Row>
+        
+            
+
+        
+
+        </Container>   
 
     );
 
